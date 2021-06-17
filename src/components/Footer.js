64f849eb1cca6ecb2ed10231ece1da0b4d0391ff
nutchanonc <edu.nutchanon@gmail.com>
2021-06-17 23:48:55 +0700

    Link Github logo to project's github repository

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer(props) {
     return (
         <>
             <div className="footer d-flex justify-content-center align-items-center">
-                <a href="https://github.com/nutchanonc" target="_blank"><img src="/GitHub-Mark-120px-plus.png" width="30px"/> </a>
+                <a href="https://github.com/nutchanonc/cryptoprices" target="_blank"><img src="/GitHub-Mark-120px-plus.png" width="30px"/> </a>
             </div>
         </>
     )
